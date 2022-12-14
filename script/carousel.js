66323af4f8c7775de0f978d58eef9c5f4794cc56
Katja Levring <katj6233@stud.kea.dk>
2022-12-14 11:37:57 +0100

    team side

--- script/carousel.js
+++ script/carousel.js
@@ -1,58 +1,166 @@
 // karrusel
-const carousel = document.querySelector('#carousel');
+const carouselSally = document.querySelector("#carousel-sally");
+const carouselEmily = document.querySelector("#carousel-emily");
+const carouselTania = document.querySelector("#carousel-tania");
 
 //Knapper
-const backButton = document.querySelector('#carousel_back_button');
-const forwardButton = document.querySelector('#carousel_forward_button');
+const backButtonSally = document.querySelector("#carousel_back_button_sally");
+const forwardButtonSally = document.querySelector(
+  "#carousel_forward_button_sally"
+);
+const backButtonEmily = document.querySelector("#carousel_back_button_emily");
+const forwardButtonEmily = document.querySelector(
+  "#carousel_forward_button_emily"
+);
+const backButtonTania = document.querySelector("#carousel_back_button_tania");
+const forwardButtonTania = document.querySelector(
+  "#carousel_forward_button_tania"
+);
 
+// indsaet billeder i carousel og giv dem id til navigation Sally
+for (let i = 1; i <= 10; i++) {
+  const imageSally = document.createElement("img");
 
-// indsaet billeder i carousel og giv dem id til navigation
-for (let i = 1 ; i <= 10; i++) {
-    const image = document.createElement('img');
+  imageSally.classList.add("carousel-item");
+  imageSally.src =
+    "/img/web_img/teams_content/sally/Sally_tat_img" + i + ".png";
+  imageSally.alt = "Billede af";
+  imageSally.id = "carousel-item" + i;
 
-    image.classList.add('carousel-item');
-    image.src = "/img/web_img/teams_content/sally/Sally_tat_img" + i + ".png";
-    image.alt = "Billede af";
-    image.id = "carousel-item" + i;
-
-    carousel.appendChild(image);
+  carouselSally.appendChild(imageSally);
 }
 
-let lastImageId = 1;
+// indsaet billeder i carousel og giv dem id til navigation Emily
+for (let i = 1; i <= 10; i++) {
+  const imageEmily = document.createElement("img");
+
+  imageEmily.classList.add("carousel-item");
+  imageEmily.src =
+    "/img/web_img/teams_content/emily/Emily_tat_img" + i + ".png";
+  imageEmily.alt = "Billede af";
+  imageEmily.id = "carousel-item" + i;
+
+  carouselEmily.appendChild(imageEmily);
+}
+
+// indsaet billeder i carousel og giv dem id til navigation Tania
+for (let i = 1; i <= 10; i++) {
+  const imageTania = document.createElement("img");
+
+  imageTania.classList.add("carousel-item");
+  imageTania.src =
+    "/img/web_img/teams_content/tania/Tania_tat_img" + i + ".png";
+  imageTania.alt = "Billede af";
+  imageTania.id = "carousel-item" + i;
+
+  carouselTania.appendChild(imageTania);
+}
+
+let lastImageIdSally = 1;
+let lastImageIdEmily = 1;
+let lastImageIdTania = 1;
 
 // event tryk frem
-forwardButton.addEventListener('click', () => {
-    
-    // stop hvis sidste billede
-    if(lastImageId >= 10) return;
-    
-    // opdater aktive billede
-    const nextImageId = lastImageId + 1;
+forwardButtonSally.addEventListener("click", () => {
+  // stop hvis sidste billede
+  if (lastImageId >= 10) return;
 
-    // scroll til naeste billede
-    const nextImage = document.querySelector('#carousel-item' + nextImageId);
+  // opdater aktive billede
+  const nextImageId = lastImageId + 1;
 
-    const scrollPosition = nextImage.getBoundingClientRect().left
+  // scroll til naeste billede
+  const nextImage = document.querySelector("#carousel-item" + nextImageId);
 
-    carousel.scrollBy({left: scrollPosition, behavior: 'smooth'});
-    lastImageId = nextImageId;
-})
+  const scrollPosition = nextImage.getBoundingClientRect().left;
+
+  carousel.scrollBy({ left: scrollPosition, behavior: "smooth" });
+  lastImageId = nextImageId;
+});
 
 // event tryk tilbage
-backButton.addEventListener('click', () => {
+backButtonSally.addEventListener("click", () => {
+  // stop hvis foerste  billede
+  if (lastImageId <= 1) return;
 
-        // stop hvis foerste  billede
-        if(lastImageId <= 1) return;
-    
-        // opdater aktive billede
-        const nextImageId = lastImageId - 1;
-    
-        // scroll til naeste billede
-        const nextImage = document.querySelector('#carousel-item' + nextImageId);
-    
-        const scrollPosition = nextImage.getBoundingClientRect().left
-    
-        carousel.scrollBy({left: scrollPosition, behavior: 'smooth'});
+  // opdater aktive billede
+  const nextImageId = lastImageId - 1;
 
-        lastImageId = nextImageId;
-})
+  // scroll til naeste billede
+  const nextImage = document.querySelector("#carousel-item" + nextImageId);
+
+  const scrollPosition = nextImage.getBoundingClientRect().left;
+
+  carousel.scrollBy({ left: scrollPosition, behavior: "smooth" });
+
+  lastImageId = nextImageId;
+});
+
+// event tryk frem
+forwardButtonEmily.addEventListener("click", () => {
+  // stop hvis sidste billede
+  if (lastImageId >= 10) return;
+
+  // opdater aktive billede
+  const nextImageId = lastImageId + 1;
+
+  // scroll til naeste billede
+  const nextImage = document.querySelector("#carousel-item" + nextImageId);
+
+  const scrollPosition = nextImage.getBoundingClientRect().left;
+
+  carousel.scrollBy({ left: scrollPosition, behavior: "smooth" });
+  lastImageId = nextImageId;
+});
+
+// event tryk tilbage
+backButtonEmily.addEventListener("click", () => {
+  // stop hvis foerste  billede
+  if (lastImageId <= 1) return;
+
+  // opdater aktive billede
+  const nextImageId = lastImageId - 1;
+
+  // scroll til naeste billede
+  const nextImage = document.querySelector("#carousel-item" + nextImageId);
+
+  const scrollPosition = nextImage.getBoundingClientRect().left;
+
+  carousel.scrollBy({ left: scrollPosition, behavior: "smooth" });
+
+  lastImageId = nextImageId;
+});
+
+// event tryk frem
+forwardButtonTania.addEventListener("click", () => {
+  // stop hvis sidste billede
+  if (lastImageIdTania >= 10) return;
+
+  // opdater aktive billede
+  const nextImageId = lastImageIdTania + 1;
+
+  // scroll til naeste billede
+  const nextImage = document.querySelector("#carousel-item" + nextImageId);
+
+  const scrollPosition = nextImage.getBoundingClientRect().left;
+
+  carousel.scrollBy({ left: scrollPosition, behavior: "smooth" });
+  lastImageIdTania = nextImageId;
+});
+
+// event tryk tilbage
+backButtonTania.addEventListener("click", () => {
+  // stop hvis foerste  billede
+  if (lastImageId <= 1) return;
+
+  // opdater aktive billede
+  const nextImageId = lastImageId - 1;
+
+  // scroll til naeste billede
+  const nextImage = document.querySelector("#carousel-item" + nextImageId);
+
+  const scrollPosition = nextImage.getBoundingClientRect().left;
+
+  carousel.scrollBy({ left: scrollPosition, behavior: "smooth" });
+
+  lastImageId = nextImageId;
+});
